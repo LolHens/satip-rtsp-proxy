e6aa40604f9f37973d6f4914af4bf7ba32e12024
Pierre Kisters <pierrekisters@gmail.com>
2020-12-05 18:00:13 +0100

    modify transport source and destination

--- src/main/scala/de/lolhens/rtsp/proxy/Main.scala
+++ src/main/scala/de/lolhens/rtsp/proxy/Main.scala
@@ -1,5 +1,6 @@
 package de.lolhens.rtsp.proxy
 
+import cats.effect.concurrent.Ref
 import cats.effect.{Blocker, ExitCode}
 import fs2._
 import fs2.io.tcp.SocketGroup
@@ -23,32 +24,102 @@ object Main extends TaskApp {
       case s"$host:$port" if port.toIntOption.isDefined => new InetSocketAddress(host, port.toInt)
     }.getOrElse(new InetSocketAddress("0.0.0.0", destinationSocketAddress.getPort))
 
+    val rtspClientHost = env.get("RTSP_CLIENT_HOST")
+    val rtspServerHost = env.get("RTSP_SERVER_HOST")
+
+    val debug = env.get("DEBUG").exists(_.toBoolean)
+
     Blocker[Task].use { blocker =>
       SocketGroup[Task](blocker).use { socketGroup =>
         socketGroup.serverResource[Task](listenSocketAddress).use {
           case (serverSocket, clientResources) =>
-            (Stream.eval(Task(logger.info("Server started on " + serverSocket))) >>
-              clientResources.map { clientResource =>
-                Stream.resource(clientResource).flatMap { proxyClient =>
-                  Stream.eval(proxyClient.remoteAddress).map(remoteAddress => logger.info("Client connected from " + remoteAddress)) >>
-                    Stream.resource(socketGroup.client(destinationSocketAddress)).flatMap { destinationClient =>
-                      Stream.eval(destinationClient.remoteAddress).map(remoteAddress => logger.info("Proxy connection established to " + remoteAddress)) >>
-                        proxyClient.reads(8192)
-                          .through(destinationClient.writes())
-                          .concurrently(
-                            destinationClient.reads(8192)
-                              //.through(text.utf8Decode)
-                              //.through(text.lines)
-                              //.interleave(Stream.constant("\n"))
-                              //.through(text.utf8Encode)
-                              .through(proxyClient.writes())
-                          )
-                    }
-                }.handleErrorWith { throwable =>
-                  throwable.printStackTrace()
-                  Stream(())
-                }
-              }.parJoin(100)).compile.drain
+            logger.info("Server started on " + serverSocket)
+
+            clientResources.map { clientResource =>
+              Stream.resource(clientResource).flatMap { proxyClient =>
+                for {
+                  proxyClientRemoteAddress <- Stream.eval(proxyClient.remoteAddress).map {
+                    case inetSocketAddress: InetSocketAddress => inetSocketAddress
+                  }
+                  _ = logger.info("Client connected from " + proxyClientRemoteAddress)
+                  destinationClient <- Stream.resource(socketGroup.client(destinationSocketAddress))
+                  destinationClientRemoteAddress <- Stream.eval(destinationClient.remoteAddress).map {
+                    case inetSocketAddress: InetSocketAddress => inetSocketAddress
+                  }
+                  _ = logger.info("Proxy connection established to " + destinationClientRemoteAddress)
+                  setupReadRef <- Stream.eval(Ref.of(false))
+                  setupSentRef <- Stream.eval(Ref.of(false))
+                  _ <-
+                    proxyClient.reads(8192)
+                      .through(text.utf8Decode)
+                      .through(text.lines)
+                      .evalMap { line =>
+                        if (line.startsWith("SETUP "))
+                          for {
+                            _ <- setupReadRef.set(true)
+                            _ <- setupSentRef.set(true)
+                          } yield
+                            line
+                        else
+                          for {
+                            setupRead <- setupReadRef.get
+                            line <-
+                              if (setupRead && line.startsWith("Transport:")) {
+                                for {
+                                  _ <- setupReadRef.set(false)
+                                } yield {
+                                  val host = rtspClientHost.getOrElse(proxyClientRemoteAddress.getHostString)
+                                  val s"Transport:$transport" = line
+                                  val params = transport.trim.split(";")
+                                  "Transport: " +
+                                    (params.filterNot(_.startsWith("destination=")) :+ s"destination=$host")
+                                      .mkString(";")
+                                }
+                              } else
+                                Task.now(line)
+                          } yield
+                            line
+                      }
+                      .debug(logger = if (debug) string => println("> " + string) else _ => ())
+                      .interleave(Stream.constant("\r\n"))
+                      .through(text.utf8Encode)
+                      .through(destinationClient.writes())
+                      .concurrently(
+                        destinationClient.reads(8192)
+                          .through(text.utf8Decode)
+                          .through(text.lines)
+                          .evalMap { line =>
+                            for {
+                              setupSent <- setupSentRef.get
+                              line <-
+                                if (setupSent && line.startsWith("Transport:")) {
+                                  for {
+                                    _ <- setupSentRef.set(false)
+                                  } yield {
+                                    val host = rtspServerHost.getOrElse(destinationSocketAddress.getHostString)
+                                    val s"Transport:$transport" = line
+                                    val params = transport.trim.split(";")
+                                    "Transport: " +
+                                      (params.filterNot(_.startsWith("source=")) :+ s"source=$host")
+                                        .mkString(";")
+                                  }
+                                } else
+                                  Task.now(line)
+                            } yield
+                              line
+                          }
+                          .debug(logger = if (debug) string => println("< " + string) else _ => ())
+                          .interleave(Stream.constant("\r\n"))
+                          .through(text.utf8Encode)
+                          .through(proxyClient.writes())
+                      )
+                } yield
+                  ()
+              }.handleErrorWith { throwable =>
+                throwable.printStackTrace()
+                Stream(())
+              }
+            }.parJoin(100).compile.drain
         }
       }
     }.as(ExitCode.Success)
